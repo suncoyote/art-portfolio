2ddc0710badb223722576adc84b11d3c8b7f7892
Aspen <175189330+suncoyote@users.noreply.github.com>
2024-07-23 22:58:41 -0400

    Display all png files in gallery-images

--- index.php
+++ index.php
@@ -7,8 +7,21 @@
 </head>
 <body>
     <?php 
-    include 'navigation.html'; 
+        include 'navigation.html'; 
 
     ?>
+
+
+
+<?php $dir = 'gallery-images/';
+        $files = glob( $dir . '*.png');
+        foreach( $files as $file) {
+            $filename = pathinfo( $file, PATHINFO_FILENAME);
+            $description = file_exists( $filename.'txt') ? file_get_contents( $filename.'txt') : '';
+            echo '<div class="artwork"><img src="' . $file . '">'.$description.'</div>';
+            
+            
+        } 
+?> 
 </body>
 </html>

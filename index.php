ca948cba35b681a8986c776bc489644e3386526c
Aspen <175189330+suncoyote@users.noreply.github.com>
2024-07-23 23:03:43 -0400

    Add CSS stylesheet

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ASPEN EYES</title>
+    <link rel="stylesheet" href ="style.css">
 </head>
 <body>
     <?php 
@@ -16,9 +17,7 @@
 <?php $dir = 'gallery-images/';
         $files = glob( $dir . '*.png');
         foreach( $files as $file) {
-            $filename = pathinfo( $file, PATHINFO_FILENAME);
-            $description = file_exists( $filename.'txt') ? file_get_contents( $filename.'txt') : '';
-            echo '<div class="artwork"><img src="' . $file . '">'.$description.'</div>';
+            echo '<img class="artwork" src="' . $file . '">';
             
             
         } 

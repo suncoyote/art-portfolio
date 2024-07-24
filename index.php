31b5c114acff62546aa2dfe70df2abf6dee8d8df
Aspen <175189330+suncoyote@users.noreply.github.com>
2024-07-24 06:51:47 -0400

    Add lightgallery to display images

--- index.php
+++ index.php
@@ -3,10 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ASPEN EYES</title>
+    <title>ASPEN EYES | Portfolio</title>
     <link rel="stylesheet" href ="style.css">
-</head>
+    <link rel="stylesheet" href="js/node_modules/glightbox/src/postcss/glightbox.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.2/css/lightgallery.min.css" integrity="sha512-F2E+YYE1gkt0T5TVajAslgDfTEUQKtlu4ralVq78ViNxhKXQLrgQLLie8u1tVdG2vWnB3ute4hcdbiBtvJQh0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    </head>
+
 <body>
+
     <div class="header">
     <div class="header-content">
         
@@ -17,18 +22,30 @@
     </div>
 
 
-<div class="gallery">
+<div class="gallery" id="gallery">
     <?php $dir = 'gallery-images/';
                 $files = glob( $dir . '*.png');
                 foreach( $files as $file) {
                     echo '
-                    <div class="gallery-item"> 
-                        <img src="' . $file . '">
+                   
+                    <div class="gallery-item" id="lightgallery">
+                    
+                        <a href="'.$file.'" ><img src="' . $file . '" class="item">
+                 
                     </div>';
+                    
                     
                     
                 } 
         ?> 
 </div>
 </body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.2/lightgallery.min.js" integrity="sha512-jEJ0OA9fwz5wUn6rVfGhAXiiCSGrjYCwtQRUwI/wRGEuWRZxrnxoeDoNc+Pnhx8qwKVHs2BRQrVR9RE6T4UHBg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script>
+       
+        lightGallery(document.getElementById('gallery'), {
+            selector: '.item'
+        })
+
+    </script>
 </html>
